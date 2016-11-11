2b3d0e37aa1c65d9269e9f070f139dd082efd3f5
bakyt98 <bakit_isimova@mail.ru>
2016-11-12 01:21:04 +0600

    2 hw

---
+++ 2.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+
+int a[100];
+int n,k;
+
+void print() {
+	for (int i = 0; i < n; i ++) {
+		if (a[i] < 10) cout << a[i];
+		else cout << (char)(a[i] + 'a' - 10);
+	}
+	cout << "\n";
+}
+
+void rec(int x) {
+	if (x >= n) {
+		print();
+		return;
+	}
+	for (a[x]=k; a[x]>=0; a[x]--){
+	    rec(x+1); }
+}
+
+int main() {
+	cin >> n>>k;
+	rec(0);
+}
